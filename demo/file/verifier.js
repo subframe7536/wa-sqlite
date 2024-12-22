337125e586a4830f37e760ca71db91b6268f8570
Song <1667077010@qq.com>
2024-12-22 12:44:18 +0800

    Update verifier.js

--- demo/file/verifier.js
+++ demo/file/verifier.js
@@ -6,6 +6,8 @@ const SEARCH_PARAMS = new URLSearchParams(location.search);
 const IDB_NAME = SEARCH_PARAMS.get('idb') ?? 'sqlite-vfs';
 const DB_NAME = SEARCH_PARAMS.get('db') ?? 'sqlite.db';
 
+console.log('Start verify');
+
 (async function() {
   const module = await SQLiteESMFactory();
   const sqlite3 = SQLite.Factory(module);
@@ -13,14 +15,15 @@ const DB_NAME = SEARCH_PARAMS.get('db') ?? 'sqlite.db';
   const vfs = await IDBBatchAtomicVFS.create(IDB_NAME, module);
   // @ts-ignore
   sqlite3.vfs_register(vfs, true);
-
+  
   const db = await sqlite3.open_v2(DB_NAME, SQLite.SQLITE_OPEN_READWRITE, IDB_NAME);
 
   const results = []
   await sqlite3.exec(db, 'PRAGMA integrity_check;', (row, columns) => {
+    console.log('Get result:', row);
     results.push(row[0]);
   });
   await sqlite3.close(db);
-  
+  console.log('Start post message');
   postMessage(results);
 })();
